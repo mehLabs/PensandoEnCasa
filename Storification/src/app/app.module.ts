74a3ee8b2383ee31d5e63d09faed5977ee909472
Hugo <hugo.sud.ste@gmail.com>
2022-03-28 16:03:20 -0300

    ordening files

--- Storification/src/app/app.module.ts
+++ Storification/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArticlesComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
